9d1c9c495ae431726b33bb495b82ca5458ec261b
pedro <pedrolgiasson@outlook.com>
2020-12-06 18:19:15 -0300

    commit on master

--- game.cpp
+++ game.cpp
@@ -12,10 +12,10 @@ int main(){
  
   //Texturas do fundo,bolinha,nave,asteroide
     Texture t1,t2,t3,t4;
-    t1.loadFromFile("images/block01.png");
-    t2.loadFromFile("images/background.jpg");
-    t3.loadFromFile("images/ball.png");
-    t4.loadFromFile("images/paddle.png");
+    t1.loadFromFile("img/block01.png");
+    t2.loadFromFile("img/background.jpg");
+    t3.loadFromFile("img/ball.png");
+    t4.loadFromFile("img/paddle.png");
 
     Sprite sBackground(t2), sBall(t3), sPaddle(t4); //sprites
     sPaddle.setPosition(300,440); //posicionar a nave
@@ -39,19 +39,50 @@ int main(){
   
   
   while (app.isOpen()) //quando abre o jogo
-    {
-       Event e;//cria um evento
-       while (app.pollEvent(e))
-       {
-         if (e.type == Event::Closed) //fechar o jgo
-             app.close();
-       }
+  {
+     Event ev;//cria um evento
+     while (app.pollEvent(ev))
+     {
+       if (ev.type == Event::Closed) //fechar o jgo e tecla esc pra close
+       app.close();
+       else if(ev.type == Event::KeyPressed){
+           if(ev.key.code==Keyboard::Escape)
+           app.close();
+     }
+     }
     
+    //se a bolinha a bolinha bater no asteroide ele vai sumindo
+    x+=dx;
+    for (int i=0;i<n;i++)
+        if ( FloatRect(x+3,y+3,6,6).intersects(block[i].getGlobalBounds()) ) {
+          block[i].setPosition(-100,0); dx=-dx; //tira o bloco e muda a direção da bolinha
+          
+        }
+
+    y+=dy;
+    for (int i=0;i<n;i++)
+        if ( FloatRect(x+3,y+3,6,6).intersects(block[i].getGlobalBounds()) ){
+          block[i].setPosition(-100,0); dy=-dy;//tira o bloco e muda a direção da bolinha
+           
+        }
+    //muda a direção da bolinha
+    if (x<0 || x>520)  dx=-dx;
+    if (y<0 || y>450)  dy=-dy;
+
+    if (Keyboard::isKeyPressed(Keyboard::Right)) sPaddle.move(7,0);//movimentação
+    if (Keyboard::isKeyPressed(Keyboard::Left)) sPaddle.move(-7,0);//movimentação
+
+    if ( FloatRect(x,y,12,12).intersects(sPaddle.getGlobalBounds()) ) dy=-(rand()%5+2);//quando bate na nave
+
+    sBall.setPosition(x,y);//bolinea posição inicial
+
     app.clear();
     app.draw(sBackground);//desenha o fundo
     app.draw(sBall); // desenha o bolinea
     app.draw(sPaddle);//desenha a nave
-      app.display();
+    for (int i=0;i<n;i++) //desenha o asteroide e posi
+    app.draw(block[i]);
+    app.display();
 
    }
 
