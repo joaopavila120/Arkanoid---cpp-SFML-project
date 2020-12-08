ed17177e1134f87f8bcbb40c17add5a4b465b95b
Arthur Jesus <72893926+ArthurwJesus@users.noreply.github.com>
2020-12-08 20:42:34 -0300

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <ctime>
 #include <fstream>
-#include "nick.cpp"
 
 using namespace std;
 
 int main()
 {
-  ofstream arquivoS;
-  time_t timer;
+  ofstream arquivoS; //saindo do programa e indo pro arquivo
+  time_t timer; //representar tempos
   struct tm *horarioLocal;
 
   time(&timer); // Obtem informações de data e hora
@@ -16,17 +15,18 @@ int main()
 
   int dia = horarioLocal->tm_mday;
   int mes = horarioLocal->tm_mon + 1;
-  int ano = horarioLocal->tm_year + 1900;
+  int ano = horarioLocal->tm_year + 1900; //obter as informações da data
 
   int hora = horarioLocal->tm_hour;
   int min  = horarioLocal->tm_min;
-  int sec  = horarioLocal->tm_sec;
+  int sec  = horarioLocal->tm_sec; //obter horario
 
-  arquivoS.open("Ranking.txt",ios::app);
+  arquivoS.open("Ranking.txt",ios::app); //Abrir o arquivo txt e app é pra operações apenas no final
 
   arquivoS <<"Nick:  " << "\n" << "Horário: " << hora << ":" << min << ":" << sec <<"\n" <<"Data: " << dia  << "/" << mes << "/" << ano <<" \n"<<"Pontos:" <<"       "<< endl;
+// o que ira sair dentro do arquivo 
 
-  arquivoS.close();
+  arquivoS.close(); //fechar arquivo
 
 
   return 0;
