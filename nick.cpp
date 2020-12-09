d95b4e50d964ba99ac2003496b567d169a715e9b
joaopavila120 <67394574+joaopavila120@users.noreply.github.com>
2020-12-08 22:25:32 -0300

    sistema de save e nickname

--- nick.cpp
+++ nick.cpp
@@ -7,33 +7,34 @@ using namespace std;
 
 int main()
 {
-fstream arquivoE;
-  char opt='s';
-  string nick,linha;
+fstream arquivoE;//Define se é entrada ou saida com espeficiaçãoes
+  char opt='s'; //dar a opção mas não irá ter a opção 
+  string nick,linha; //variaveis 
 
-  arquivoE.open("Nicks.txt",ios::out | ios::app);
+  arquivoE.open("Nicks.txt",ios::out | ios::app); //abrir arquivo,app é pra operações apenas no final e out é para sair do sistema e ir pro arquivo
+
   
-  while((opt=='s')or (opt=='S')){
-      cout << "Digite seu nick:  ";
-      cin >> nick;
-      arquivoE << nick <<"\n";
-      cout <<"\nDigite[n]";
-      cin >> opt; 
-      system("clear");
+  while((opt=='s')or (opt=='S')){ //enquanto o opt foi s ou S ele vai permitir digiar novos nicks
+      cout << "Digite seu nick:  "; 
+      cin >> nick; //recebendo
+      arquivoE << nick <<"\n";//adicionar no arquivo 
+      cout <<"\nSalvar[n]"; // digitar N para salvar
+      cin >> opt; //vai receber a opção
+      system("clear"); //limpar a tela de saida
   }
-  arquivoE.close();
+  arquivoE.close();//fechar os arquivo 
 
-  arquivoE.open("Nicks.txt",ios::in);
-  cout<< "Jogadores" << endl;
+  arquivoE.open("Nicks.txt",ios::in);//ler o arquivo e entrar no sistema
+  cout<< "Jogadores" << endl;//mostrar o nome dos jogadores
 
-  if(arquivoE.is_open()){
-      while(getline(arquivoE,linha)){
-          cout << linha << endl;
+  if(arquivoE.is_open()){//se abriu ele vai mostrar no terminal tbm
+      while(getline(arquivoE,linha)){//vai ler do arquivo e armazenar na linha
+          cout << linha << endl;//mostrnado...
       }
-      arquivoE.close();
+      arquivoE.close();//fecha depois de mostrar
   }
   else{
-      cout<<"Erro" << endl;
+      cout<<"Erro" << endl; //senão der aparecerá erro
   }
     return 0;
 }
